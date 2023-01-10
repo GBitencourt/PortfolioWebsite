bcbd0bbd87bba202701c4a06fab8bcfe4ddef322
GBitencourt <gBit64@outlook.com>
2023-01-10 16:38:44 -0300

    Feature: A few styles and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
+import { About } from "./components/About";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+    </>
+  );
 };
 
 export default App;
